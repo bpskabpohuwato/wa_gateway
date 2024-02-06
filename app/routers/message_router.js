c156a13bed081ea75bf4ccb14a2e1bbae660a797
bpskabpohuwato <132872592+bpskabpohuwato@users.noreply.github.com>
2024-02-06 13:21:02 +0800

    Update message_router.js

--- app/routers/message_router.js
+++ app/routers/message_router.js
@@ -2,10 +2,14 @@ const { Router } = require("express");
 const {
   sendMessage,
   sendBulkMessage,
+  sendDocument,
+  sendImage,
 } = require("../controllers/message_controller");
 const MessageRouter = Router();
 
 MessageRouter.all("/send-message", sendMessage);
 MessageRouter.all("/send-bulk-message", sendBulkMessage);
+MessageRouter.all("/send-document", sendDocument);
+MessageRouter.all("/send-image", sendImage);
 
 module.exports = MessageRouter;
