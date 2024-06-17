0d3b7f2e8ed62f369a5e5173e5e372bcb77ebd29
bpskabpohuwato <132872592+bpskabpohuwato@users.noreply.github.com>
2024-06-18 05:39:07 +0800

    Update index.js
    Add LogRouter

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const MessageRouter = require("./message_router");
+const LogRouter = require("./log_router");
 const SessionRouter = require("./session_router");
+const MessageRouter = require("./message_router");
 
 const MainRouter = Router();
 
+MainRouter.use(LogRouter);
 MainRouter.use(SessionRouter);
 MainRouter.use(MessageRouter);
 
